a5116f49db851b777a6a980909ec15601a45eaf5
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-03-10 23:19:05 +0300

    2

--- C++ (обучение)/ООП/ООП.Перегрузка арифметических операторов/ООП.Перегрузка арифметических операторов.cpp
+++ C++ (обучение)/ООП/ООП.Перегрузка арифметических операторов/ООП.Перегрузка арифметических операторов.cpp
@@ -1,27 +1,61 @@
 ﻿// ООП.Перегрузка арифметических операторов.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 #include <iostream>
-
+#define print(x) std::cout<<x<<std::endl
 class Point {
     int x;
     int y;
     int z;
 
 public:
-    Point(int x, int y, int z) {
+    Point(int x = 0, int y = 0, int z = 0) {
         this->x = x;
         this->y = y;
         this->z = z;
     }
-    Point() {
-        this->x = 0;
-        this->y = 0;
-        this->z = 0;
+
+    Point operator +(const Point& other) {
+        Point temp;
+        temp.x = this->x + other.x;
+        temp.y = this->y + other.y;
+        temp.z = this->z + other.z;
+        return temp;
+    }
+
+    int GetX() {
+        return this->x;
+    }
+    int GetY() {
+        return this->y;
+    }
+    int GetZ() {
+        return this->z;
+    }
+    void SetX(int x){
+        this->x = x;
+    }
+    void SetY(int y) {
+        this->y = y;
+    }
+    void SetZ(int z) {
+        this->z = z;
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Point dot1(5, 2);
+    Point dot2(0, 0, 4);
+    Point dot3 = dot1 + dot2;
+
+    print(dot3.GetX());
+    print(dot3.GetY());
+    print(dot3.GetZ());
+
+    dot3 = dot3 + dot3 + dot3;
+
+    print(dot3.GetX());
+    print(dot3.GetY());
+    print(dot3.GetZ());
 }
 
