bc61dc9c773efb5470cc341a29c4dab0007ae38d
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-03-12 22:32:46 +0300

    3

---
+++ C++ (обучение)/Лаб.работа/Иванова.СПП/Иванова.СПП.cpp
@@ -0,0 +1,32 @@
+﻿// Иванова.СПП.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int n,temp_n,x = 0,fact=1;
+	const int bs = 6, dg = 2, one = 1;
+
+	std::cout << "Введите число итераций: ";
+	std::cin >> n;
+
+	while (!(n == 1))
+	{
+		temp_n = 1;
+		fact = 1;
+		while (!(temp_n == n)) {
+			fact = fact + (temp_n *fact);
+			temp_n = temp_n + one;
+		}
+
+	x = x + ((pow(bs, n) * (pow(n, dg) - one)) / (fact));
+
+	n = n - one;
+	}
+	std::cout << "Ответ: " << x << std::endl;
+
+}
+
