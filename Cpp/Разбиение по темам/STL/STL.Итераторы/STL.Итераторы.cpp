d196f8039cca76064074d70f6c9891961bd09149
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-03-21 02:10:36 +0300

    11

---
+++ Cpp/Разбиение по темам/STL/STL.Итераторы/STL.Итераторы.cpp
@@ -0,0 +1,72 @@
+﻿// STL.Итераторы.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    vector<int>vectr = { 5,4,52,61 };
+    vector<int>::iterator it;
+
+    it = vectr.begin();
+
+    cout << *it << endl;
+    *it = 141;
+    cout << *it << endl;
+    cout << "\n------------\n" << endl;
+
+    for (vector<int>::const_iterator i = vectr.cbegin(); i != vectr.cend(); i++)
+        cout << *i << endl;
+    
+    cout << "\n------------\n" << endl;
+
+
+    for (vector<int>::reverse_iterator i = vectr.rbegin(); i != vectr.rend(); i++)
+        cout << *i << endl;
+
+    cout << "\n------------\n" << endl;
+
+    vector<int> vec1{ 2,14,68,781 };
+    vector<int>::iterator iter1 = vec1.begin();
+
+    advance(iter1, 2);
+    cout << *iter1 << endl;
+
+    cout << "\n------------\n" << endl;
+
+    vector<int> vec2{ 2,14,68,781,51,24,76,97 };
+    vector<int>::iterator iter2 = vec2.begin();
+
+    advance(iter2, 2);
+    vec2.insert(iter2, 999);
+
+    for (vector<int>::iterator i = vec2.begin(); i != vec2.end(); i++)
+        cout << *i << endl;
+    cout << "\n------------\n" << endl;
+
+    vector<int>::iterator it_erase = vec2.begin();
+    /*vec2.erase(it_erase);
+
+    for (vector<int>::iterator i = vec2.begin(); i != vec2.end(); i++)
+        cout << *i << endl;
+    cout << "\n------------\n" << endl;*/
+
+    vec2.erase(it_erase,it_erase+4);
+
+    for (vector<int>::iterator i = vec2.begin(); i != vec2.end(); i++)
+        cout << *i << endl;
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
+
---
+++ Cpp/Разбиение по темам/ООП/ООП. Умные указатели (smart pointer)/Умные указатели (smart pointer).cpp
@@ -0,0 +1,32 @@
+﻿// Умные указатели (smart pointer).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#define print(x) std::cout << x << std::endl
+
+template <typename T>
+class SmartPointer {
+public:
+    SmartPointer(T *ptr) {
+        this->ptr = ptr;
+    }
+    ~SmartPointer() {
+        delete ptr;
+    }
+    T& operator *() {
+        return *ptr;
+    }
+private:
+    T* ptr;
+};
+
+
+int main()
+{
+
+    SmartPointer<int>pntr = new int(5);
+
+    *pntr = 10;
+    std::cout << *pntr << std::endl;
+}
+
