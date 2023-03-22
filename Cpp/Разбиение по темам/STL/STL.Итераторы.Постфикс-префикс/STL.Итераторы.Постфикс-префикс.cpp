dce3a3f784a9c724f9199bea3f1ccc7279cf6e3d
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-03-22 23:40:12 +0300

    12

---
+++ Cpp/Разбиение по темам/STL/STL.Итераторы.Постфикс-префикс/STL.Итераторы.Постфикс-префикс.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <list>
+#define print(x) std::cout<<x<<std::endl;
+using namespace std;
+
+template <typename T>
+void PrintList(const list<T> &lst) {
+
+    for (auto i = lst.cbegin(); i != lst.cend(); ++i)
+        print(*i);
+
+}
+
+int main()
+{
+    list<int> lst = { 5,12,6,5,5,3 };
+    auto iter = lst.begin();
+  //  lst.erase(iter);
+    advance(iter, 3);
+    lst.insert(iter, 500);
+    PrintList(lst);
+    iter++;
+    ++iter;
+    
+
+
+}
+
