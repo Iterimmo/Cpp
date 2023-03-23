58298e271f79ae7a4a008cbe7f30b7c7ed506b14
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-03-23 03:45:57 +0300

    13

---
+++ Cpp/Разбиение по темам/STL/STL.forward_list/STL.forward_list.cpp
@@ -0,0 +1,35 @@
+﻿// STL.forward_list.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <forward_list>
+#include <iostream>
+#include <list>
+#define print(x) std::cout<<x<<std::endl;
+
+using namespace std;
+
+template<typename T>
+void PrintList(list<T>& fl) {
+
+    for(const auto& i:fl)
+        print(i)
+
+}
+int main()
+{
+    
+    forward_list<int> frlst;
+
+    auto iter_frlt = frlst.begin();
+
+
+
+
+
+
+
+    print("\n-----------\n");
+    list<int> lst = { 5,4,2,4,14,66 };
+
+    PrintList(lst);
+}
+
---
+++ Cpp/Разбиение по темам/Процедурное программирование/ПП. Цикл foreach.range-based циклы/ПП. Цикл foreach.range-based циклы.cpp
@@ -0,0 +1,30 @@
+﻿// ПП. Цикл foreach.range-based циклы.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <list>
+#define print(x) std::cout <<x<<std::endl
+#define vector std::vector
+int main()
+{
+
+	vector<int>vec = { 5,7,9,11,13,15 };
+
+	int arr[] = { 5,5,2,4,2,1,24,6,3 };
+
+	for(auto var:arr) {
+		print(var);
+	}
+	//
+	print('\n');
+	for (auto &var : vec) {
+		var += 100;
+		print(var);
+	}
+	print(std::endl);
+	for (const auto& var : vec) {
+		print(var);
+	}
+
+}
