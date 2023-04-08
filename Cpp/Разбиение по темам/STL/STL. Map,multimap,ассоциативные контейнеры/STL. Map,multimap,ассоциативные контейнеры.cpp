f1a26e5fca0c8c49a4699d37beca3df8f574a264
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-04-08 15:12:17 +0300

    22

---
+++ Cpp/Разбиение по темам/STL/STL. Map,multimap,ассоциативные контейнеры/STL. Map,multimap,ассоциативные контейнеры.cpp
@@ -0,0 +1,33 @@
+﻿// STL. Map,multimap,ассоциативные контейнеры.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+#define print(x) std::cout<<x<<std::endl
+#define newpart print("\n--------------\n")
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	pair<int, string> pr(1,"lays");
+	
+	cout << pr.first << "\t" << pr.second;
+	newpart;
+
+	map<int, string> mp({ {1, "Alisha"} });
+	mp.insert(make_pair(3, "Intel"));
+	mp.emplace(2, "AMD"); // <-- использовать для добавления элементов
+	mp.emplace(2, "Leha?"); // <-- элемент не будет добавлен, т.к. 2 одинаковых ключа.
+	auto it = mp.find(44);
+
+	it != mp.end() ? print(it->second):print("Элемент не найден");
+
+	newpart;
+
+	return 0;
+
+}
+
