0c3bb5c220d1870fb6dcefa27ab95b62202b2e67
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-05-03 00:45:28 +0300

    03.05.2023

---
+++ Cpp/Разбиение по темам/Многопоточность/TH. Потоки с параметрами/TH. Потоки с параметрами.cpp
@@ -0,0 +1,34 @@
+﻿// TH. Потоки с параметрами.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void DoWork(int a, int b) {
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(400));
+    std::cout << "\tWorking..." << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    std::cout << "\ta + b = " << a + b << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    std::cout << "\tWork is over" << std::endl << std::endl;
+
+}
+int main()
+{
+    std::thread th(DoWork,5,7);
+
+
+    for (size_t i = 0; true ; i++) {
+        std::cout << "ID main thread " << std::this_thread::get_id() << "   " << (i+1) << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+
+
+
+    th.join();
+}
+
---
+++ Cpp/Разбиение по темам/Многопоточность/TH.Возврат результата из потока по ссылке/TH.Возврат результата из потока по ссылке.cpp
@@ -0,0 +1,27 @@
+﻿// TH.Возврат результата из потока по ссылке.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+
+void Sum(int a, int b) {
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+	std::cout << "Working..." << std::endl;
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+	std::cout << "a + b = " << a + b << std::endl;
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+	std::cout << "Work is ending..." << std::endl;
+
+}
+
+
+int main()
+{
+
+
+}
