b55bbb5bfb8da8bc49a150d286b62deb6d903bab
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-05-04 18:04:17 +0300

    04.05.2023

--- Cpp/Разбиение по темам/Многопоточность/TH.Возврат результата из потока по ссылке/TH.Возврат результата из потока по ссылке.cpp
+++ Cpp/Разбиение по темам/Многопоточность/TH.Возврат результата из потока по ссылке/TH.Возврат результата из потока по ссылке.cpp
@@ -6,22 +6,35 @@
 #include <chrono>
 
 
-void Sum(int a, int b) {
+void Double(int &a) {
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-	std::cout << "Working..." << std::endl;
+	std::cout <<"Thread "<<std::this_thread::get_id() << " is working..." << std::endl;
+
+	a *= 2;
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-	std::cout << "a + b = " << a + b << std::endl;
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-	std::cout << "Work is ending..." << std::endl;
+	std::cout << "Thread " << std::this_thread::get_id() << " is ending..." << std::endl;
 
 }
 
 
 int main()
 {
+	setlocale(LC_ALL, "ru");
+	
+	int q = 5;
 
+	std::thread th(Double,std::ref(q));
+
+	for (size_t i = 0; i<8; i++) {
+
+		std::cout << "ID main thread " << std::this_thread::get_id() << "  " << (i + 1) << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+	}
+
+	th.join();
+	std::cout <<"------------------------- " << q << std::endl;
 
 }
