b56cc5714e9df44b3a9b40d956901c85345d1d0e
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-05-08 23:47:03 +0300

    08.05.2023

---
+++ Cpp/Разбиение по темам/ООП/ООП. Модификаторы доступа (public,protected,private)/ООП. Модификаторы доступа (public,protected,private).cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+class A {
+public:
+    int a1 = 1;
+private:
+    int a2 = 2;
+protected:
+    int a3 = 3;
+};
+
+class B : public A {
+public:
+    void PrintA() {
+        std::cout << a1 << " " << a3 << " " << std::endl; // a2 - недоступен.
+   }
+
+};
+int main()
+{
+
+    B b;
+    b.PrintA();
+   
+}
+
--- Cpp/Разбиение по темам/ООП/ООП. Наследование/ООП. Наследование.cpp
+++ Cpp/Разбиение по темам/ООП/ООП. Наследование/ООП. Наследование.cpp
@@ -2,19 +2,55 @@
 //
 
 #include <iostream>
+#include <string>
+
+class Human {
+    int age = 30;
+    std::string name;
+
+public:
+    int GetAge() {
+        return age;
+    }
+    void SetName(std::string name) {
+        this->name = name;
+    }
+};
+
+class Student: public Human{
+public:
+    std::string group;
+    void Learn() {
+        std::cout << "learning...." << std::endl;
+    }
+};
+
+class ExtmuralStudent : public Student {
+public:
+    void Learn() {
+        std::cout << "Ходить в универ = миф" << std::endl;
+    }
+
+};
+
+class Professor: public Human {
+public:
+    std::string subject;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+
+    Student st;
+    st.SetName("Alex");
+    st.group = "IAS-20";
+    st.Learn();
+    ExtmuralStudent emst;
+
+    emst.SetName("NeAlex"); // из Human через Student
+    emst.group = "!IAS-20"; // из Student
+
+    emst.Learn();
+    std::cout << emst.GetAge() << "     " << (emst.GetAge() == st.GetAge()) << std::endl;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
