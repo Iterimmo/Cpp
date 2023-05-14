5de74c8eb708ed28e9262189ca01f8ac395dab24
DESKTOP-SKJLD4A\Iterimmo <godik_f@mail.ru>
2023-05-14 21:47:42 +0300

    14.05.2023

---
+++ Cpp/Набиваем руку/Ravesli/Rav 20 - многофайловые программы/Rav 20 - многофайловые программы.cpp
@@ -0,0 +1,11 @@
+﻿
+#include <iostream>
+
+int add(int x, int y);
+int getInteger();
+int main()
+{
+
+    std::cout << "The sum of x and y is: " << add(getInteger(), getInteger()) << std::endl;
+    return 0;
+}
--- Cpp/Набиваем руку/Ravesli/Rav.114 - спецификаторы/Rav.114 - спецификаторы.cpp
+++ Cpp/Набиваем руку/Ravesli/Rav.114 - спецификаторы/Rav.114 - спецификаторы.cpp
@@ -20,62 +20,120 @@
 
 */
 // 2 класс, который реализует функционал стека
+/*
+1. закрытый целочисленный фиксированный массив длиной 10 элементов;
 
+2. закрытое целочисленное значение для отслеживания длины стека;
+
+3. открытый метод с именем reset(), который будет инициализировать значением 0 длину и все значения элементов;
+
+4. открытый метод с именем push(), который будет добавлять значение в стек.Метод push() должен возвращать значение false, если массив уже заполнен, в противном случае — true;
+
+5. открытый метод с именем pop() для возврата значений из стека.Если в стеке нет значений, то должен выводиться стейтмент assert;
+
+6. открытый метод с именем print(), который будет выводить все значения стека. 
+*/
 
 #include <iostream>
+#include<cassert>
+
 class Stack {
 private:
-    int current_lenght = 0;
     int stack[10];
+    int current_lenght = 0;
     bool initialized = false;
 
-    bool Limit() {
-        if (current_lenght != 10) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    bool NoLimit() {
+        return (this->stack[9] == 0);
     }
 public:
     void Reset() {
         for (size_t i = 0; i < 10; i++) {
-            stack[i] = 0;
+            this->stack[i] = 0;
         }
-        bool initialized = true;
+        this->initialized = true;
+        this->current_lenght = 0;
 
     }
+
     void PrintStack() {
         if (initialized) {
-            for (auto i : stack) {
-                std::cout << i << std::endl;
+            for (const auto& i : this->stack) {
+                std::cout << i;
             }
+            std::cout << std::endl;
         }
         else {
             std::cout << "Стек неинициализирован" << std::endl;
         }
     }
 
-    bool Push() {
-        if (Limit()) {
-            for (int i = 1; i < 10; i++) {
-                stack[i + 1] = stack[i];
+    bool Push(int val) {
+        if (!initialized) {
+            Reset();
+        }
+        if (NoLimit()) {
+
+            for (int i = 9; i > 0; i--) {
+                this->stack[i] = this->stack[i - 1];
+                if (i == 1) {
+                    this->stack[i - 1] = val;
+                }
             }
+            this->current_lenght++;
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
-};
+    void Pop() {
 
+        if (!initialized) {
+            Reset();
+        }
+
+        assert(current_lenght > 0, && "stack is empty");
+
+        for (int i = 0; i < 9; i++) {
+            this->stack[i] = this->stack[i + 1];
+            if (i == 8) {
+                this->stack[i + 1] = 0;
+            }
+        }
+        this->current_lenght--;
+       
+    }
+
+    int Len() {
+        return this->current_lenght;
+    }
+};
 
 int main()
 {
-  //  Stack stack;
-  //  stack.Reset();
-    int stack[10];
-    for (auto i : stack) {
-        std::cout << i << std::endl;
-    }
-    std::cout << sizeof(stack) << std::endl;
-    
-}
+    setlocale(LC_ALL, "ru");
+     Stack stack;
+     stack.Reset();
+     stack.PrintStack();
+     stack.Push(5);
+
+     stack.PrintStack();
+     stack.Pop();
+     stack.Push(5);
+     stack.Push(5);
+     stack.Push(5);
+
+
+     stack.PrintStack();
+
+     std::cout << stack.Len() << std::endl;
+     stack.Reset();
+     std::cout << stack.Len() << std::endl;
+     stack.PrintStack();
+
+
+};
+
 
